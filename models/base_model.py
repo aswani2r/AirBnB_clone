4d092d4a119ea1775fcd6c429a15a4e00a1614b1
aswani2r <ruudaswani26@gmail.com>
2024-03-01 11:37:35 +0300

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -18,14 +18,14 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
-        models.storage.save(self)
+        storage.save(self)
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
