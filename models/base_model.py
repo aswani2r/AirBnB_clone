47739c80df936798555a55111b5efeb1e71fa391
aswani2r <ruudaswani26@gmail.com>
2024-06-27 05:45:31 -0700

    first models

---
+++ models/base_model.py
@@ -0,0 +1,21 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        self.updated_at = datetime.now()
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_rep = self.__dict__copy()
+        dict_rep['__class__'] = self.__class__.__name__
+        dict_rep['created_at'] = self.created_at.isoformat()
+        dict_rep['updated_at'] = self.updated_at.isoformat()
+        return dict_rep
