173856dcacdc8c208b6a9ec600f8e176adc8f0e9
aswani2r <ruudaswani26@gmail.com>
2024-06-27 06:21:08 -0700

    new changes 2

--- models/base_model.py
+++ models/base_model.py
@@ -5,17 +5,21 @@ from datetime import datetime
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
 
     def __str__(self):
-        self.updated_at = datetime.now()
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__,
+                self.id,
+                self.__dict__
+                )
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now().isoformat()
 
     def to_dict(self):
-        dict_rep = self.__dict__copy()
+        dict_repr = {key: value for key, value in self.__dict__.itemss()}
         dict_rep['__class__'] = self.__class__.__name__
         dict_rep['created_at'] = self.created_at.isoformat()
         dict_rep['updated_at'] = self.updated_at.isoformat()
