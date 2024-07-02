57bdf748423fdcfc4d00e27a630ab289adabdc66
aswani2r <ruudaswani26@gmail.com>
2024-07-02 16:28:01 -0700

    Models 5

--- models/base_model.py
+++ models/base_model.py
@@ -5,26 +5,32 @@ base class for basemodels contained here.
 import uuid
 import sys
 from datetime import datetime
+import models
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self,*args, **kwargs):
+        if kwargs:
+            for kkey, value in kwargs.item():
+                if key =="created_at" or key == "updated_at":
+                    setattr(self, key, datetime.striptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now().isoformat()
         self.updated_at = datetime.now().isoformat()
+        models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(
-                self.__class__.__name__,
-                self.id,
-                self.__dict__
-                )
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now().isoformat()
+        models.storage.save()
 
     def to_dict(self):
         dict_repr = self.__dict__.copy()}
-        dict_rep['__class__'] = self.__class__.__name__
+        dict_rep['__class__'] = type(self).__class__.__name__
         dict_rep['created_at'] = self.created_at.isoformat()
         dict_rep['updated_at'] = self.updated_at.isoformat()
-        return dict_rep
+        return obj_dict
