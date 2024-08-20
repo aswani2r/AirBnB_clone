24b616df6695e53edf84359245316ca258c36567
aswani2r <ruudaswani26@gmail.com>
2024-08-20 10:57:45 +0000

    basemodel updated

--- models/base_model.py
+++ models/base_model.py
@@ -8,28 +8,51 @@ import sys
 import models
 
 class BaseModel:
+    """Base class for all models"""
+
     def __init__(self, *args, **kwargs):
+        """Initialization of a Base instance.
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
         if kwargs:
+            dtime_format = '%Y-%m-%dT%H:%M:%S.%f'
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                if key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(
+                        kwargs['created_at'], dtime_format)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        kwargs['updated_at'], dtime_format)
                 else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)  # as instructed also in task 5
 
     def __str__(self):
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        """Returns a readable string representation
+        of BaseModel instances"""
+
+        clsName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
 
     def save(self):
+        """Updates the public instance attribute updated_at
+        with the current datetime"""
+
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = type(self).__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """Returns a dictionary that contains all
+        keys/values of the instance"""
+        my_dict = self.__dict__.copy()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['__class__'] = self.__class__.__name__
